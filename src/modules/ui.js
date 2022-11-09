209bbeb916daae4d0b09e86b736393e2c72d1e61
Bryan Miller <bmilcs@yahoo.com>
2022-11-09 17:32:27 -0500

    Refactor fetch / async functions & rm localStorage

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,10 +1,9 @@
 import "../scss/index.scss";
 import containerize from "./utils/containerize";
 import makeElement from "./utils/make-element";
+import * as APP from "./app";
 
 export const renderWeather = (obj) => {
-  console.log(obj);
-
   const leftColumn = containerize(
     "left-col",
     makeElement("img", "weather-img", "Weather Icon", "", obj.imageURL)
@@ -21,6 +20,41 @@ export const renderWeather = (obj) => {
   );
 
   const container = containerize("weather-container", leftColumn, rightColumn);
-
   document.querySelector("body").appendChild(container);
+};
+
+export const renderForm = () => {
+  const form = makeElement("form", "zipcode-form", "", "zipcode-form");
+  form.addEventListener("submit", changeLocation);
+
+  const inputLabel = makeElement("label", "input-label", "Zip Code:");
+  inputLabel.htmlFor = "zipcode-input";
+
+  const inputTextbox = makeElement(
+    "input",
+    "zipcode-input",
+    "",
+    "zipcode-input"
+  );
+  inputTextbox.required = true;
+
+  const button = makeElement(
+    "button",
+    "change-zipcode-btn",
+    "Submit",
+    "change-zipcode-btn"
+  );
+
+  const inputContainer = containerize(form, inputLabel, inputTextbox, button);
+  document.querySelector("body").appendChild(inputContainer);
+};
+
+const changeLocation = (e) => {
+  e.preventDefault();
+  const zip = e.target["zipcode-input"].value;
+  APP.changeLocation(zip);
+};
+
+const clearWeather = () => {
+  document.querySelector(".weather-container").remove();
 };
